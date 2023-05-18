5213c8c3cf52669b1f8891e599f6521f059a49d1
DAB0mB <emanor6@gmail.com>
2023-05-17 23:19:19 -0400

    checkpoint

--- plugin/transformers/program_transformer.ts
+++ plugin/transformers/program_transformer.ts
@@ -1,9 +1,12 @@
-import { NodePath } from '@babel/traverse';
+import { NodePath, Scope } from '@babel/traverse';
 import * as t from '@babel/types';
 import { Config } from '../config.js';
 import { HookTransformer } from './hook_transformers/hook_transformer.js';
 import { createHookTransformer } from './hook_transformers/index.js';
 import { Transformer } from './transformer.js';
+import { weakMemo } from '../utils/function.js';
+import { z } from 'zod';
+import { CodeGenerator } from '@babel/generator';
 
 export class ProgramTransformer extends Transformer {
   readonly moduleImportDeclarations: t.ImportDeclaration[] = [];
@@ -24,21 +27,100 @@ export class ProgramTransformer extends Transformer {
   }
 
   traverse() {
+    const depsByAutorun = new Map<NodePath, string[]>();
+    let autorunImportSpecifiers!: Set<NodePath>;
+
     this.path.traverse({
-      ImportDeclaration: (path) => {
-        if (path.node.source.value === this.config.getModuleName('react-useless')) {
-          this.moduleImportDeclarations.push(path.node);
-        }
+      Program: {
+        enter: (program) => {
+          autorunImportSpecifiers = getAutorunImportSpecifiers(program.scope);
+        },
+        exit: (program) => {
+          for (const [autorun, deps] of depsByAutorun) {
+            autorun.replaceWith(
+
+            ),
+          }
+        },
       },
+      CallExpression: (callExpression) => {
+        const callback = callExpression.get('arguments')[0];
+        if (!callback.isFunctionExpression() && !callback.isArrowFunctionExpression()) return;
 
-      CallExpression: (path) => {
-        const hook = createHookTransformer(path, this.config);
+        const autorun = callExpression.get('arguments')[1];
+        if (!autorun.isIdentifier()) return;
 
-        if (hook) {
-          hook.traverse();
-          this.hooks.push(hook);
-        }
+        const autorunBinding = callExpression.scope.getBinding(autorun.node.name);
+        if (!autorunBinding || !autorunImportSpecifiers.has(autorunBinding.path)) return;
+
+        const deps = getFunctionExpressionDeps(callback);
+        depsByAutorun.set(autorun, Array.from(deps));
       },
     });
   }
+}
+
+function getAutorunImportSpecifiers(scope: Scope) {
+  const specifiers = new Set<NodePath<t.ImportSpecifier>>();
+
+  scope.path.traverse({
+    ImportDeclaration: (importDeclaration) => {
+      if (importDeclaration.get('source').get('value') !== 'react-autorun') return;
+
+      for (const specifier of importDeclaration.get('specifiers')) {
+        if (specifier.isImportSpecifier() && specifier.get('imported').isIdentifier({ name: 'autorun' })) {
+          specifiers.add(specifier);
+        }
+      }
+    },
+  });
+
+  return specifiers;
+}
+
+function getFunctionExpressionDeps(path: NodePath<t.ArrowFunctionExpression | t.FunctionExpression>) {
+  const deps = new Set<string>();
+
+  path.traverse({
+    MemberExpression: (memberExpression) => {
+      if (t.isMemberExpression(memberExpression.parent)) return;
+
+      let object: t.Node = memberExpression.node;
+      const props: string[] = [];
+      while (t.isMemberExpression(object)) {
+        props.unshift(object.computed ? `[${generate(object.property)}]` : `.${(object.property as t.Identifier).name}`);
+        object = object.object;
+      }
+
+      if (!t.isIdentifier(object)) return;
+      if (!path.scope.getOwnBinding(object.name)) return;
+
+      let dep = object.name;
+      while (props.length > 1) {
+        dep += props.shift();
+      }
+
+      deps.add(dep + props.shift());
+      if (t.isCallExpression(memberExpression.parentPath.node)) {
+        deps.add(dep);
+      }
+    },
+
+    Identifier: (identifier) => {
+      if (t.isMemberExpression(identifier.parent)) return;
+
+      const dep = identifier.node.name
+      if (!path.scope.getOwnBinding(dep)) return;
+
+      deps.add(dep);
+    },
+  });
+
+  return deps;
+}
+
+function generate(ast: t.Node) {
+  return new CodeGenerator(ast, {
+    minified: true,
+  }).generate().code;
 }
