dcb3237e4211d09cdee4faff172b004dcaffa439
DAB0mB <emanor6@gmail.com>
2023-05-20 20:05:52 -0400

    Complete babel tests

--- babel/babel.test.ts
+++ babel/babel.test.ts
@@ -1,24 +1,23 @@
-import { transform } from '@babel/core';
-import traverse, { NodePath, Visitor, visitors } from '@babel/traverse';
+import { CodeGenerator } from '@babel/generator';
+import { parse } from '@babel/parser';
+import traverse, { visitors } from '@babel/traverse';
 import * as t from '@babel/types';
 import { equal } from 'node:assert';
 import { test } from 'node:test';
 import plugin from './babel';
-import { parse } from '@babel/parser';
-import { CodeGenerator } from '@babel/generator';
 
 test('babel plugin', async (t) => {
-  await t.test('transforms autorun identifier into a call expression that returns an array of dependencies based on the block scope and the hook callback', () => {
+  await t.test('transforms autorun identifier into a call expression that returns an array of dependencies based on the block scope and the hook callback', { only: true }, () => {
     const input = `
       import { autorun } from 'react-autorun';
 
-      const a = 1;
+      let a;
 
       {
-        const b = 2;
+        let b;
 
         useHook(() => {
-          const c = 3;
+          let c;
 
           a;
           b;
@@ -29,6 +28,128 @@ test('babel plugin', async (t) => {
     `;
 
     equal(getTransformedAutorunCode(input), 'autorun(() => [b])');
+  });
+
+  await t.test('includes a complete member expression', () => {
+    const input = `
+      import { autorun } from 'react-autorun';
+
+      {
+        let object;
+
+        useHook(() => {
+          object.member.expression;
+        }, autorun);
+      }
+    `;
+
+    equal(getTransformedAutorunCode(input), 'autorun(() => [object.member.expression])');
+  });
+
+  await t.test('includes caller if callee is a member expression', () => {
+    const input = `
+      import { autorun } from 'react-autorun';
+
+      {
+        let caller;
+
+        useHook(() => {
+          caller.callee();
+        }, autorun);
+      }
+    `;
+
+    equal(getTransformedAutorunCode(input), 'autorun(() => [caller.callee, caller])');
+  });
+
+  await t.test('transforms autorun alias', () => {
+    const input = `
+      import { autorun as autorunAlias } from 'react-autorun';
+
+      let a;
+
+      {
+        let b;
+
+        useHook(() => {
+          let c;
+
+          a;
+          b;
+          c;
+          d;
+        }, autorunAlias);
+      }
+    `;
+
+    equal(getTransformedAutorunCode(input, 'autorunAlias'), 'autorunAlias(() => [b])');
+  });
+
+  await t.test('does not transform autorun if it was not imported', () => {
+    const input = `
+      let a;
+
+      {
+        let b;
+
+        useHook(() => {
+          let c;
+
+          a;
+          b;
+          c;
+          d;
+        }, autorun);
+      }
+    `;
+
+    equal(getTransformedAutorunCode(input), null);
+  });
+
+  await t.test('handles commonjs', () => {
+    const input = `
+      const { autorun } = require('react-autorun');
+
+      let a;
+
+      {
+        let b;
+
+        useHook(() => {
+          let c;
+
+          a;
+          b;
+          c;
+          d;
+        }, autorun);
+      }
+    `;
+
+    equal(getTransformedAutorunCode(input), 'autorun(() => [b])');
+  });
+
+  await t.test('handles commonjs alias', () => {
+    const input = `
+      const { autorun: autorunAlias } = require('react-autorun');
+
+      let a;
+
+      {
+        let b;
+
+        useHook(() => {
+          let c;
+
+          a;
+          b;
+          c;
+          d;
+        }, autorunAlias);
+      }
+    `;
+
+    equal(getTransformedAutorunCode(input, 'autorunAlias'), 'autorun(() => [b])');
   });
 });
 
@@ -57,5 +178,5 @@ function getTransformedAutorunCode(input: string, autorunIdName = 'autorun') {
     return autorunNode;
   }
 
-  return new CodeGenerator(ast).generate().code;
+  return new CodeGenerator(autorunNode).generate().code;
 }
