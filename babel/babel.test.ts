bb81cd736434155b2972dde5f7d686f155a99ebc
DAB0mB <emanor6@gmail.com>
2023-06-12 10:05:26 -0400

    Use fixtures for babel tests

--- babel/babel.test.ts
+++ babel/babel.test.ts
@@ -1,237 +1,46 @@
 import { CodeGenerator } from '@babel/generator';
 import { parse } from '@babel/parser';
 import traverse, { visitors } from '@babel/traverse';
-import * as t from '@babel/types';
+import { Node } from '@babel/types';
 import { equal } from 'node:assert';
+import { readFile, readdir } from 'node:fs/promises';
 import { test } from 'node:test';
 import plugin from './babel';
 
+type Config = typeof defaultConfig;
+
+const defaultConfig = {
+  autorun_symbol: 'autorun' as string,
+};
+
 test('babel plugin', async (t) => {
-  await t.test('transforms autorun identifier into a call expression that returns an array of dependencies based on the block scope and the hook callback', () => {
-    const input = `
-      import { autorun } from 'react-autorun';
+  const fixtures = await readdir('test/fixture');
 
-      let a;
-
-      {
-        let b;
-
-        useHook(() => {
-          let c;
-
-          a;
-          b;
-          c;
-          d;
-        }, autorun);
-      }
-    `;
-
-    equal(getTransformedAutorunCode(input), 'autorun(() => [b])');
-  });
-
-  await t.test('includes a complete member expression', () => {
-    const input = `
-      import { autorun } from 'react-autorun';
-
-      {
-        let object;
-
-        useHook(() => {
-          object.member.expression;
-        }, autorun);
-      }
-    `;
-
-    equal(getTransformedAutorunCode(input), 'autorun(() => [object?.member?.expression])');
-  });
-
-  await t.test('includes no dependency duplicates', () => {
-    const input = `
-      import { autorun } from 'react-autorun';
-
-      {
-        let object;
-
-        useHook(() => {
-          object['member'].expression;
-        }, autorun);
-      }
-    `;
-
-    equal(getTransformedAutorunCode(input), 'autorun(() => [object?.["member"]?.expression])');
-  });
-
-  await t.test('includes caller if callee is a member expression', () => {
-    const input = `
-      import { autorun } from 'react-autorun';
-
-      {
-        let caller;
-
-        useHook(() => {
-          caller.callee();
-        }, autorun);
-      }
-    `;
-
-    equal(getTransformedAutorunCode(input), 'autorun(() => [caller, caller?.callee])');
-  });
-
-  await t.test('includes computed properties of member expressions', () => {
-
-  });
-
-  await t.test('transforms autorun alias', () => {
-    const input = `
-      import { autorun as autorunAlias } from 'react-autorun';
-
-      let a;
-
-      {
-        let b;
-
-        useHook(() => {
-          let c;
-
-          a;
-          b;
-          c;
-          d;
-        }, autorunAlias);
-      }
-    `;
-
-    equal(getTransformedAutorunCode(input, 'autorunAlias'), 'autorunAlias(() => [b])');
-  });
-
-  await t.test('does not transform autorun if it was not imported', () => {
-    const input = `
-      let a;
-
-      {
-        let b;
-
-        useHook(() => {
-          let c;
-
-          a;
-          b;
-          c;
-          d;
-        }, autorun);
-      }
-    `;
-
-    equal(getTransformedAutorunCode(input), null);
-  });
-
-  await t.test('does not transform ignore calls', () => {
-    const input = `
-      import { autorun } from 'react-autorun';
-
-      let a;
-
-      {
-        let b;
-
-        autorun.ignore(b);
-
-        useHook(() => {
-          let c;
-
-          a;
-          b;
-          c;
-          d;
-        }, autorun);
-      }
-    `;
-
-    equal(getTransformedAutorunCode(input), 'autorun(() => [b])');
-  });
-
-  await t.test('handles commonjs', () => {
-    const input = `
-      const { autorun } = require('react-autorun');
-
-      let a;
-
-      {
-        let b;
-
-        useHook(() => {
-          let c;
-
-          a;
-          b;
-          c;
-          d;
-        }, autorun);
-      }
-    `;
-
-    equal(getTransformedAutorunCode(input), 'autorun(() => [b])');
-  });
-
-  await t.test('handles commonjs alias', () => {
-    const input = `
-      const { autorun: autorunAlias } = require('react-autorun');
-
-      let a;
-
-      {
-        let b;
-
-        useHook(() => {
-          let c;
-
-          a;
-          b;
-          c;
-          d;
-        }, autorunAlias);
-      }
-    `;
-
-    equal(getTransformedAutorunCode(input, 'autorunAlias'), 'autorunAlias(() => [b])');
-  });
-
-  await t.test('not includes some dependencies that were yielded from React hooks', () => {
-    const input = `
-      import { useState, useReducer, useRef } from 'react';
-      import { autorun } from 'react-autorun';
-
-      {
-        const [state, setState] = useState();
-        const [reducedState, dispatch] = useReducer();
-        const ref = useRef();
-
-        useHook(() => {
-          state;
-          setState;
-          reducedState;
-          dispatch;
-          ref;
-          ref.current;
-        }, autorun);
-      }
-    `;
-
-    equal(getTransformedAutorunCode(input, 'autorun'), 'autorun(() => [state, reducedState])');
-  });
+  await Promise.all(fixtures.map(async fixture => {
+    const dirname = `test/fixture/${fixture}`;
+    const input = await readFile(`${dirname}/input.ts`).then(file => file.toString().trim());
+    const output = await readFile(`${dirname}/output.ts`).then(file => file.toString().trim());
+    const config: Config = await readFile(`${dirname}/config.json`)
+      .then(
+        file => ({ ...defaultConfig, ...JSON.parse(file.toString()) }),
+        () => ({ ...defaultConfig }),
+      );
+    await t.test(fixture, () => {
+      equal(getTransformedAutorunCode(input, config), output);
+    })
+  }));
 });
 
-function getTransformedAutorunCode(input: string, autorunIdName = 'autorun') {
+function getTransformedAutorunCode(input: string, config: Config) {
   const ast = parse(input, {
     sourceType: 'module',
     plugins: ['optionalChaining'],
   });
 
-  let autorunNode: t.Node | null = null;
+  let autorunNode: Node | null = null;
   const throwAutorunNode = {
     CallExpression(path) {
-      if (path.get('callee').isIdentifier(({ name: autorunIdName }))) {
+      if (path.get('callee').isIdentifier(({ name: config.autorun_symbol }))) {
         throw (autorunNode = path.node);
       }
     },
@@ -248,7 +57,7 @@ function getTransformedAutorunCode(input: string, autorunIdName = 'autorun') {
   }
 
   if (autorunNode == null) {
-    return autorunNode;
+    return 'AUTORUN_NOT_FOUND';
   }
 
   return new CodeGenerator(autorunNode).generate().code;
